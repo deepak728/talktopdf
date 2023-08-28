a4876e5e631fd631cc102d62f238cda4128aea30
deepak728 <npe.deepak@gmail.com>
2023-08-28 16:50:59 +0530

    Creating project

---
+++ talktopdf/src/test/java/com/example/talktopdf/TalktopdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talktopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalktopdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
