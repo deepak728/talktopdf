8b2fc8972b6f7428dbd17a27f0b2095a9c560bd5
deepak728 <npe.deepak@gmail.com>
2023-08-29 01:46:41 +0530

    added db migration

--- talktopdf/src/main/java/com/example/talktopdf/Controller/TestController.java
+++ talktopdf/src/main/java/com/example/talktopdf/Controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.talktopdf.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
     @GetMapping("/test")
     public String test(){
         return "Testing the application";
+
     }
+
 
 }
--- talktopdf/src/main/java/com/example/talktopdf/TalktopdfApplication.java
+++ talktopdf/src/main/java/com/example/talktopdf/TalktopdfApplication.java
@@ -1,17 +1,27 @@
 package com.example.talktopdf;
 
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import liquibase.integration.spring.SpringLiquibase;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class TalktopdfApplication {
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource dataSource) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(dataSource);
+		liquibase.setChangeLog("classpath:db/changelog/db.changelog-1.0.xml");
+		return liquibase;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TalktopdfApplication.class, args);
 
 	}
+
 
 }
